2d6bef5fb443a495681d329d53470d7af4d3886e
Shivjanak <patelshivjanak@gmail.com>
2025-12-30 15:12:32 +0530

    admin order

--- frontend/src/pages/AdminOrders.jsx
+++ frontend/src/pages/AdminOrders.jsx
@@ -9,7 +9,7 @@ function AdminOrders() {
   }, []);
 
   const fetchOrders = async () => {
-    const res = await axios.get("http://localhost:5000/api/orders", {
+    const res = await axios.get("https://laptop-store-mern.onrender.com/api/orders", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("adminToken")}`
       }
